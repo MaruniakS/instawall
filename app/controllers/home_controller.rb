4b01795b4b41588eeb9bbbe7477b7323c9c20b7e
STASILu4 <stasilu4@gmail.com>
2016-01-15 20:51:24 +0200

    home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,18 +2,16 @@ class HomeController < ApplicationController
   require 'rest_client'
   require 'json'
 
-  CALLBACK_URL = "http://localhost:3000/home/callback"
   def index
-    #@response = HTTParty.get("https://api.instagram.com/v1/tags/rest/media/recent?access_token=2552384949.a0b5e2e.4562a4d19eff4123b473fb25364e7a2f")
-    #@response = HTTParty.get("https://api.instagram.com/v1/users/self/media/liked/?access_token=2552384949.a0b5e2e.4562a4d19eff4123b473fb25364e7a2f")
   end
 
   def connect
-    redirect_to Instagram.authorize_url(:redirect_uri => CALLBACK_URL)
+    #render :text => home_callback_url
+    redirect_to Instagram.authorize_url(:redirect_uri => home_callback_url)
   end
 
   def callback
-    response = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK_URL)
+    response = Instagram.get_access_token(params[:code], :redirect_uri => home_callback_url)
     session[:access_token] = response.access_token
     redirect_to action: 'nav'
   end
@@ -22,6 +20,7 @@ class HomeController < ApplicationController
   end
 
   def tags
+
     client = Instagram.client(:access_token => session[:access_token])
     @tags = client.tag_search('rest')
     @media = client.tag_recent_media(@tags[0].name)
