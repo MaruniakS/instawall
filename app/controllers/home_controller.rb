22b6112a99d8f2bee35b8db031cf60fd3b960ddd
STASILu4 <stasilu4@gmail.com>
2016-01-21 20:21:45 +0200

    try to fix render on heroku

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
   end
 
   def tags
-    render :partial => 'shared/tags'
+    render :partial => 'shared/tags',:formats => :html
   end
 
   def get_photos
