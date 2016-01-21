ada181afd6557885adc363015a8b4aafd789febe
STASILu4 <stasilu4@gmail.com>
2016-01-21 16:51:11 +0200

    fixes of rendering

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,14 +10,10 @@ class HomeController < ApplicationController
   def get_photos
     tag_name = params[:tag]
     max_id = params[:max_id]
+    save_tag(tag_name) if max_id.blank?
     cache = Cache.get_response(current_user.id, request.original_url)
     render json: cache and return if cache
-    if max_id.blank?
-      save_tag(tag_name)
-      media = current_user.instagram_client.tag_recent_media(tag_name, count: 5)
-    else
-      media = current_user.instagram_client.tag_recent_media(tag_name, count: 5, max_id: max_id)
-    end
+    media = max_id.blank? ? current_user.instagram_client.tag_recent_media(tag_name, count: 5) : current_user.instagram_client.tag_recent_media(tag_name, count: 5, max_id: max_id)
     render json: Cache.add_to_cache(current_user.id, request.original_url, result(media))
   end
 
